0a1a7090bf302c828f2d26dc996fef18a0da2901
Syddie89 <151478558+Syddie89@users.noreply.github.com>
2023-12-15 13:41:19 -0500

    Update health.v

--- health.v
+++ health.v
@@ -5,9 +5,24 @@ module health(health, clk, rst, health_update, start);
 	output reg health;
 
 always @ (posedge clk or negedge rst) 
-	if (start) 
+begin
+	/*if (!start) begin
+		health <= 1'b0;
+	end else begin 
 		health <= 1'b1;
-	if (health_update) 
+		if (health_update == 1'b1) begin 
+			health <= 1'b0;	
+		end 
+	end*/
+
+	if (!rst) begin
+				health <= 1'b1;
+		if (health_update == 1'b1) begin 
+			health <= 1'b0;
+	end else begin 
 		health <= 1'b0;	
+		end 
+	end
+	
 end 
 endmodule 
