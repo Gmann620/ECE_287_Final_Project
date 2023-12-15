4fca9afe6de243600030c5b66454f8291a8062a0
Syddie89 <151478558+Syddie89@users.noreply.github.com>
2023-12-15 11:43:23 -0500

    Update collision.v

--- collision.v
+++ collision.v
@@ -1,17 +1,15 @@
-module collision_handler(enem_grid, clk, start, health_update, user_x, user_y, enemy_x, enemy_y);
-	input [160*120-1:0]enem_grid; // 2D reigster grid representing where the bullets are on the screen
-	input clock; // default 50mhz clock on de2 board
-	input startGameEn; // FSM reset signal to reset everything 
+module collision_handler(rst, clk, start, health_update, user_x, enemy_y);
+	input clk; 
+	input start; 
+	input rst;
 	input [7:0] user_x; // 8 bit value keeping track of the user's x position on the vga
-	input [6:0] user_y; // 7 bit value keeping track of the user's y position on the vga
-	input [7:0] enemy_x; // 8 bit value keeping track of the enemy's x position on the vga
 	input [6:0] enemy_y; // 7 bit value keeping track of the enemy's y position on the vga
 
 	output reg health_update; 
 
 	always@(posedge clk or negedge rst)
 	begin
-		if(enem_grid[120*user_x+user_y] == 1'b1)begin
+		if(enemy_y[user_x] == 1'b1)begin
 			health_update <= 1'b1;
 		end
 		else begin
