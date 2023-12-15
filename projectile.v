c5c6dd24f70ead239b51c6aeb4f3346c12d901ac
Syddie89 <151478558+Syddie89@users.noreply.github.com>
2023-12-15 13:40:53 -0500

    Update projectile.v

--- projectile.v
+++ projectile.v
@@ -7,7 +7,7 @@ input shoot,
 input [9:0]playerx,
 input has_collided,
 
-output reg [9:0] projX, 
+output reg [9:0] projx, 
 output reg [9:0] projy,
 output reg exists); 
 
@@ -16,31 +16,51 @@ reg creation_flag;
 always @ (posedge clk or negedge rst) 
 begin 
 
-	if (rst) begin 
-		exists = 1'b0; 
+	if (!rst) begin 
+		//exists = 1'b0; 
+		//projx = 10'd0; 
+		//projy = 10'd0;
+	
+		if (shoot) begin 
+			creation_flag = 1'b1; 
+		end else  begin 
+			creation_flag = 1'b0;
+		
+		
+			if (creation_flag && !exists) begin 
+				exists = 1'b1; 
+				projx = playerx; 
+				projy = 10'd448; 
+			end 
+	
+			else if ( exists && projy == 10'd0) begin 
+				exists = 1'b0; 
+			end else if (exists)  begin 
+				projy = projy - 10'd4;
+			end
+		end 
+	end  else begin
+	
+	   exists = 1'b0; 
 		projx = 10'd0; 
 		projy = 10'd0; 
-	end 
 	
-	else begin 
-	if (shoot) 
-		creation_flag = 1'b1; 
-	end 
-	else begin 
-		creation_flag = 1'b0; 
-	end 
-	else if (creation_flag && !exists) begin 
-		exists = 1'b1; 
-		projx = playerx + 10'd30; 
-		projy = 10'd448; 
-	end 
+		/*if (shoot)  
+			creation_flag = 1'b1; 
+		else  
+			creation_flag = 1'b0;
+		
+		
+			if (creation_flag && !exists) begin 
+				exists = 1'b1; 
+				projx = playerx + 10'd30; 
+				projy = 10'd448; 
+			end 
 	
-	else if ( exists && projy == 10'd0) begin 
-		exists = 1'b0; 
-	
-	end 
-	else if (exists) begin 
-		projy = projy - 10'd4; 
+			else if ( exists && projy == 10'd0) 
+				exists = 1'b0; 
+			else if (exists)  
+				projy = projy - 10'd4;*/ 
 	end 
 
 end 
